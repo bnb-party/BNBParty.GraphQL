5af76c4f07cdbfc6df825c9f2bca6b9eb0c4171c
Artem Krol <artemkrol47@gmail.com>
2024-07-25 20:38:29 -0500

    Added Environment

--- src/BNBParty.GraphQLClient/GraphQlClient.cs
+++ src/BNBParty.GraphQLClient/GraphQlClient.cs
@@ -1,154 +1,152 @@
 ﻿using System;
 using System.Threading.Tasks;
-using BNBParty.GraphQLClient.Models;
 using BNBParty.GraphQLClient.Responses;
 using Flurl.Http;
 using Newtonsoft.Json;
 using static BNBParty.GraphQLClient.Generated.Types;
 
-namespace BNBParty.GraphQLClient
+namespace BNBParty.GraphQLClient;
+
+public class GraphQlClient
 {
-    public class GraphQlClient
+    private readonly string _endpoint;
+    private string _authKey;
+
+    public GraphQlClient(string endpoint)
     {
-        private readonly string _endpoint;
-        private string _authKey;
+        _endpoint = endpoint;
+        _authKey = Net.Web3.EthereumWallet.EthereumAddress.ZeroAddress;
+    }
 
-        public GraphQlClient(string endpoint)
+    public async Task<TResponse> QueryAsync<TResponse>(string query, object? variables)
+    {
+        var requestContent = new
         {
-            _endpoint = endpoint;
-            _authKey = Net.Web3.EthereumWallet.EthereumAddress.ZeroAddress;
+            query,
+            variables
+        };
+
+        try
+        {
+            var response = await _endpoint
+                .WithOAuthBearerToken(_authKey)
+                .PostJsonAsync(requestContent)
+                .ReceiveString();
+
+            var deserializedResponse = JsonConvert.DeserializeObject<TResponse>(response);
+
+            return deserializedResponse!;
         }
-
-        public async Task<TResponse> QueryAsync<TResponse>(string query, object? variables)
+        catch (FlurlHttpException ex)
         {
-            var requestContent = new
-            {
-                query,
-                variables
-            };
-
-            try
-            {
-                var response = await _endpoint
-                    .WithOAuthBearerToken(_authKey)
-                    .PostJsonAsync(requestContent)
-                    .ReceiveString();
-
-                var deserializedResponse = JsonConvert.DeserializeObject<TResponse>(response);
-
-                return deserializedResponse!;
-            }
-            catch (FlurlHttpException ex)
-            {
-                var error = await ex.GetResponseStringAsync();
-                throw new Exception($"Request failed with status code {ex.StatusCode}: {error}");
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"An error occurred: {ex.Message}");
-            }
+            var error = await ex.GetResponseStringAsync();
+            throw new Exception($"Request failed with status code {ex.StatusCode}: {error}");
         }
-
-        public async Task<bool> LoginAsync(string sign, string message)
+        catch (Exception ex)
         {
-            var mutation = Services.GraphQlQueryLoader.LoadMutation("GenerateAuthMutation.graphql");
-            var variables = new { sign, message };
-
-            var authResponse = await QueryAsync<GenerateAuthResponse>(mutation, variables);
-
-            if (authResponse.Data!.GenerateAuth != sign)
-            {
-                Console.WriteLine("Login failed, address is zero.");
-                return false;
-            }
-
-            _authKey = authResponse.Data.GenerateAuth;
-            return true;
+            throw new Exception($"An error occurred: {ex.Message}");
         }
+    }
 
-        public async Task<bool> LogoutAsync(string sign)
+    public async Task<bool> LoginAsync(string sign, string message)
+    {
+        var mutation = Services.GraphQlQueryLoader.LoadMutation("GenerateAuthMutation.graphql");
+        var variables = new { sign, message };
+
+        var authResponse = await QueryAsync<GenerateAuthResponse>(mutation, variables);
+
+        if (authResponse.Data!.GenerateAuth != sign)
         {
-            var mutation = Services.GraphQlQueryLoader.LoadMutation("DeleteAuthMutation.graphql");
-            var variables = new { sign };
-
-            var response = await QueryAsync<DeleteAuthResponse>(mutation, variables);
-
-            if (response.Data!.DeleteAuth)
-            {
-                _authKey = Net.Web3.EthereumWallet.EthereumAddress.ZeroAddress;
-                Console.WriteLine("Logged out successfully.");
-                return true;
-            }
-
-            Console.WriteLine("Logout failed.");
+            Console.WriteLine("Login failed, address is zero.");
             return false;
         }
 
-        public async Task<GetTokenResponse> GetTokenAsync(int tokenId)
+        _authKey = authResponse.Data.GenerateAuth;
+        return true;
+    }
+
+    public async Task<bool> LogoutAsync(string sign)
+    {
+        var mutation = Services.GraphQlQueryLoader.LoadMutation("DeleteAuthMutation.graphql");
+        var variables = new { sign };
+
+        var response = await QueryAsync<DeleteAuthResponse>(mutation, variables);
+
+        if (response.Data!.DeleteAuth)
         {
-            var query = Services.GraphQlQueryLoader.LoadQuery("GetTokenQuery.graphql");
-            var variables = new { tokenId };
-            return await QueryAsync<GetTokenResponse>(query, variables);
+            _authKey = Net.Web3.EthereumWallet.EthereumAddress.ZeroAddress;
+            Console.WriteLine("Logged out successfully.");
+            return true;
         }
 
-        public async Task<InsertTokenResponse> InsertTokenAsync(long chainId, string txHash)
+        Console.WriteLine("Logout failed.");
+        return false;
+    }
+
+    public async Task<GetTokenResponse> GetTokenAsync(int tokenId)
+    {
+        var query = Services.GraphQlQueryLoader.LoadQuery("GetTokenQuery.graphql");
+        var variables = new { tokenId };
+        return await QueryAsync<GetTokenResponse>(query, variables);
+    }
+
+    public async Task<InsertTokenResponse> InsertTokenAsync(long chainId, string txHash)
+    {
+        var mutation = Services.GraphQlQueryLoader.LoadMutation("InsertTokenMutation.graphql");
+        var variables = new { chainId, txHash };
+        return await QueryAsync<InsertTokenResponse>(mutation, variables);
+    }
+
+    public async Task<GenerateAuthResponse> GenerateAuthAsync(string sign, string message)
+    {
+        var mutation = Services.GraphQlQueryLoader.LoadMutation("GenerateAuthMutation.graphql");
+        var variables = new { sign, message };
+        var response = await QueryAsync<GenerateAuthResponse>(mutation, variables);
+
+
+        var query = Services.GraphQlQueryLoader.LoadQuery("MyAddressQuery.graphql");
+        var addressResponse = await QueryAsync<MyAddressResponse>(query, null);
+        if (addressResponse.Data!.MyAddress == Net.Web3.EthereumWallet.EthereumAddress.ZeroAddress)
         {
-            var mutation = Services.GraphQlQueryLoader.LoadMutation("InsertTokenMutation.graphql");
-            var variables = new { chainId, txHash };
-            return await QueryAsync<InsertTokenResponse>(mutation, variables);
+            Console.WriteLine("Login failed, address is zero.");
         }
 
-        public async Task<GenerateAuthResponse> GenerateAuthAsync(string sign, string message)
+        return response;
+    }
+
+    public async Task<UpdateTokenContentResponse> UpdateTokenContentAsync(long tokenId, OffChainData offChainData)
+    {
+        var mutation = Services.GraphQlQueryLoader.LoadMutation("UpdateTokenContentMutation.graphql");
+        var variables = new
         {
-            var mutation = Services.GraphQlQueryLoader.LoadMutation("GenerateAuthMutation.graphql");
-            var variables = new { sign, message };
-            var response = await QueryAsync<GenerateAuthResponse>(mutation, variables);
-
-
-            var query = Services.GraphQlQueryLoader.LoadQuery("MyAddressQuery.graphql");
-            var addressResponse = await QueryAsync<MyAddressResponse>(query, null);
-            if (addressResponse.Data!.MyAddress == Net.Web3.EthereumWallet.EthereumAddress.ZeroAddress)
+            tokenId,
+            offChainData = new
             {
-                Console.WriteLine("Login failed, address is zero.");
+                offChainData.content,
+                offChainData.icon,
+                offChainData.likeCounter,
+                offChainData.Discord,
+                offChainData.Telegram,
+                offChainData.Website,
+                offChainData.X
             }
+        };
 
-            return response;
-        }
+        return await QueryAsync<UpdateTokenContentResponse>(mutation, variables);
+    }
 
-        public async Task<UpdateTokenContentResponse> UpdateTokenContentAsync(long tokenId, OffChainData offChainData)
-        {
-            var mutation = Services.GraphQlQueryLoader.LoadMutation("UpdateTokenContentMutation.graphql");
-            var variables = new
-            {
-                tokenId,
-                offChainData = new
-                {
-                    offChainData.content,
-                    offChainData.icon,
-                    offChainData.likeCounter,
-                    offChainData.Discord,
-                    offChainData.Telegram,
-                    offChainData.Website,
-                    offChainData.X
-                }
-            };
+    public static string GenerateMessage(string accountAddress, long chainId)
+    {
+        var issuedAtDateTime = DateTimeOffset.UtcNow;
 
-            return await QueryAsync<UpdateTokenContentResponse>(mutation, variables);
-        }
+        var domain = GraphQlClientSetting.Domain;
+        var terms = GraphQlClientSetting.Terms;
+        var uri = GraphQlClientSetting.Uri;
+        var version = GraphQlClientSetting.Version;
+        var nonce = $"Nonce: {issuedAtDateTime.ToUnixTimeMilliseconds()}";
+        var issuedAt = $"Issued At: {issuedAtDateTime}";
+        var expirationTime = $"Expiration Time: {issuedAtDateTime.AddDays(1)}";
 
-        public static string GenerateMessage(string accountAddress, long chainId)
-        {
-            var issuedAtDateTime = DateTimeOffset.UtcNow;
-
-            var domain = GraphQlClientConfig.Domain;
-            var terms = GraphQlClientConfig.Terms;
-            var uri = GraphQlClientConfig.Uri;
-            var version = GraphQlClientConfig.Version;
-            var nonce = $"Nonce: {issuedAtDateTime.ToUnixTimeMilliseconds()}";
-            var issuedAt = $"Issued At: {issuedAtDateTime}";
-            var expirationTime = $"Expiration Time: {issuedAtDateTime.AddDays(1)}";
-
-            return $"{domain}\n{accountAddress}\n\n{terms}\n\n{uri}\n{version}\nChain ID: {chainId}\n{nonce}\n{issuedAt}\n{expirationTime}";
-        }
+        return $"{domain}\n{accountAddress}\n\n{terms}\n\n{uri}\n{version}\nChain ID: {chainId}\n{nonce}\n{issuedAt}\n{expirationTime}";
     }
 }
---
+++ src/BNBParty.GraphQLClient/GraphQlClientSetting.cs
@@ -0,0 +1,16 @@
+﻿using EnvironmentManager;
+
+namespace BNBParty.GraphQLClient;
+
+public static class GraphQlClientSetting
+{
+    private static readonly EnvManager EnvManager = new();
+
+    public static readonly string Domain = EnvManager.GetEnvironmentValue<string>("DOMAIN", true);
+
+    public static readonly string Terms = EnvManager.GetEnvironmentValue<string>("TERMS", true);
+
+    public static readonly string Uri = EnvManager.GetEnvironmentValue<string>("URI", true);
+
+    public static readonly string Version = EnvManager.GetEnvironmentValue<string>("VERSION", true);
+}
