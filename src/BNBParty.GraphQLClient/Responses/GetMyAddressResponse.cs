06a9eaec810d1b32a7ffab9771a82e9e874f120a
Artem Krol <artemkrol47@gmail.com>
2024-08-05 08:37:39 -0500

    FIxed Authorization

---
+++ src/BNBParty.GraphQLClient/Responses/GetMyAddressResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace BNBParty.GraphQLClient.Responses;
+
+internal class GetMyAddressResponse
+{
+    public DataType? Data { get; set; }
+
+    public class DataType
+    {
+        public string GetMyAddress { get; set; } = null!;
+    }
+}
